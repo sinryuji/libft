b89ef975a0ea5ed1b33f3a979f8be4fc7f45c64e
sinryuji <siler2474@gmail.com>
2022-03-11 00:16:48 +0900

    feat: ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/07 18:59:27 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/03/10 23:49:00 by hyeongki         ###   ########.fr       */
+/*   Created: 2022/03/10 22:18:26 by hyeongki          #+#    #+#             */
+/*   Updated: 2022/03/11 00:10:38 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <stdlib.h>
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+    size_t  i;
+    unsigned char tmp[n];
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
-size_t	ft_strlen(const char* c);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-
-#endif
+    if (!dest || !src)
+	return (0);
+    i = -1;
+    while (++i < n)
+	tmp[i] = *(unsigned char *)(src + i);
+    i = -1;
+    while (++i < n)
+	*(unsigned char *)(dest + i) = tmp[i];
+    return (dest);
+}
