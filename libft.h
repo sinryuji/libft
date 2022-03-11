482b868b8e83918bab8119ec148c85a68e231257
sinryuji <siler2474@gmail.com>
2022-03-11 22:21:53 +0900

    feat: ft_strlcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 18:59:27 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/03/10 23:49:00 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/03/11 13:54:20 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
