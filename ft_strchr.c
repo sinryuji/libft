04024a61becac7bdd291cab78d756ff87bcdabce
sinryuji <siler2474@gmail.com>
2022-03-28 00:38:55 +0900

    fix: ft_strchr Null Guard

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 20:21:20 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/03/22 01:49:54 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/03/28 00:36:27 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
 		if (*s == (char)c)
@@ -22,5 +24,5 @@ char	*ft_strchr(const char *s, int c)
 	}
 	if (*s == c)
 		return ((char *)s);
-	return (0);
+	return (NULL);
 }
