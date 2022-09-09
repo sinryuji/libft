e1ed18cc01b3eb75ff04ecdd99a69c0b6718b9b9
sinryuji <siler2474@gmail.com>
2022-09-09 15:16:57 +0900

    Fix : ft_strchr NULL gaurd

--- source/ft_strchr.c
+++ source/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 20:21:20 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/04/13 12:21:44 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/09/09 15:16:31 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
 		if (*s == (char)c)
