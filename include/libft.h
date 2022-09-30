dbcc0a2b6a28c1c694f4c66393a5214978082f9b
sinryuji <siler2474@gmail.com>
2022-09-30 20:36:22 +0900

    Feat : ft_perror_exit

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 18:59:27 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/09/16 17:56:25 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/09/30 20:34:55 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,5 +87,6 @@ void	ft_split_free(char **split);
 double	ft_pow(double x, double y);
 char	*get_next_line(int fd);
 int		ft_atoi_hex(char *str);
+int		ft_perror_exit(char *str);
 
 #endif
